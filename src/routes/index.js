c35e41f9375e8379ccb2afb9093583b187e4ea7b
Tran Duc Duy <tdduy291295@gmail.com>
2023-11-05 16:45:31 +0700

    Cấu hình Router/Layout cho dự án

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import { HeaderOnly } from '~/components/Layout';
+import Following from '~/pages/Following';
+import Home from '~/pages/Home';
+import Profile from '~/pages/Profile';
+import Search from '~/pages/Search';
+import Upload from '~/pages/Upload';
+
+//page
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { privateRoutes, publicRoutes };
